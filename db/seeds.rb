88c5356e2067c9230359c0d9c6645fa69f5a91fc
Veronica Lopez <v.lopezmontoro@gmail.com>
2020-02-19 15:01:25 +0100

    Yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: 'mr whao', address: 'pekin', category: 'chinese')
+restaurant2 = Restaurant.create!(name: 'pizza pronto', address: 'milan', category: 'italian')
+restaurant3 = Restaurant.create!(name: 'japablabla', address: 'tokyo', category: 'japanese')
+restaurant4 = Restaurant.create!(name: 'cuisine top', address: 'bordeaux', category: 'french')
+restaurant5 = Restaurant.create!(name: 'waffles', address: 'brussels', category: 'belgian')
